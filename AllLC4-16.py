a9f1236f2c1f17b214afbf801be5a18015bafe87
Edouard LEBAS <lebas.edouard@elebas.fr>
2018-01-18 18:17:53 +0100

    refactoring2

---
+++ AllLC4-16.py
@@ -0,0 +1,274 @@
+import time
+
+#All lower case letters
+lc_letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+
+lenght = raw_input("Lenght : ")
+
+start_time = time.time()
+if lenght == "4":
+    file = open("DIC_lcletters_4.txt","w")
+    try:
+        for a in lc_letters:
+            print a
+            for b in lc_letters:
+                for c in lc_letters:
+                    for d in lc_letters:
+                        word = a+b+c+d
+                        file.write(word+"\n")
+        print "DONE"
+        file.close()
+    except KeyboardInterrupt:
+        print "KeyboardInterrupt"
+elif lenght == "5":
+    file = open("DIC_lcletters_5.txt","w")
+    try:
+        for a in lc_letters:
+            print a
+            for b in lc_letters:
+                for c in lc_letters:
+                    for d in lc_letters:
+                        for e in lc_letters:
+                            word = a+b+c+d+e
+                            file.write(word+"\n")
+        print "DONE"
+        file.close()
+    except KeyboardInterrupt:
+        print "KeyboardInterrupt"
+elif lenght == "6":
+    file = open("DIC_lcletters_6.txt","w")
+    try:
+        for a in lc_letters:
+            print a
+            for b in lc_letters:
+                for c in lc_letters:
+                    for d in lc_letters:
+                        for e in lc_letters:
+                            for f in lc_letters:
+                                word = a+b+c+d+e+f
+                                file.write(word+"\n")
+        print "DONE"
+        file.close()
+    except KeyboardInterrupt:
+        print "KeyboardInterrupt"
+elif lenght == "7":
+    file = open("DIC_lcletters_7.txt","w")
+    try:
+        for a in lc_letters:
+            print a
+            for b in lc_letters:
+                for c in lc_letters:
+                    for d in lc_letters:
+                        for e in lc_letters:
+                            for f in lc_letters:
+                                for g in lc_letters:
+                                    word = a+b+c+d+e+f+g
+                                    file.write(word+"\n")
+        print "DONE"
+        file.close()
+    except KeyboardInterrupt:
+        print "KeyboardInterrupt"
+elif lenght == "8":
+    file = open("DIC_lcletters_8.txt","w")
+    try:
+        for a in lc_letters:
+            print a
+            for b in lc_letters:
+                for c in lc_letters:
+                    for d in lc_letters:
+                        for e in lc_letters:
+                            for f in lc_letters:
+                                for g in lc_letters:
+                                    for h in lc_letters:
+                                        word = a+b+c+d+e+f+g+h
+                                        file.write(word+"\n")
+        print "DONE"
+        file.close()
+    except KeyboardInterrupt:
+        print "KeyboardInterrupt"
+elif lenght == "9":
+    file = open("DIC_lcletters_9.txt","w")
+    try:
+        for a in lc_letters:
+            print a
+            for b in lc_letters:
+                for c in lc_letters:
+                    for d in lc_letters:
+                        for e in lc_letters:
+                            for f in lc_letters:
+                                for g in lc_letters:
+                                    for h in lc_letters:
+                                        for i in lc_letters:
+                                            word = a+b+c+d+e+f+g+h+i
+                                            file.write(word+"\n")
+        print "DONE"
+        file.close()
+    except KeyboardInterrupt:
+        print "KeyboardInterrupt"
+elif lenght == "10":
+    file = open("DIC_lcletters_10.txt","w")
+    try:
+        for a in lc_letters:
+            print a
+            for b in lc_letters:
+                for c in lc_letters:
+                    for d in lc_letters:
+                        for e in lc_letters:
+                            for f in lc_letters:
+                                for g in lc_letters:
+                                    for h in lc_letters:
+                                        for i in lc_letters:
+                                            for j in lc_letters:
+                                                word = a+b+c+d+e+f+g+h+i+j
+                                                file.write(word+"\n")
+        print "DONE"
+        file.close()
+    except KeyboardInterrupt:
+        print "KeyboardInterrupt"
+elif lenght == "11":
+    file = open("DIC_lcletters_11.txt","w")
+    try:
+        for a in lc_letters:
+            print a
+            for b in lc_letters:
+                for c in lc_letters:
+                    for d in lc_letters:
+                        for e in lc_letters:
+                            for f in lc_letters:
+                                for g in lc_letters:
+                                    for h in lc_letters:
+                                        for i in lc_letters:
+                                            for j in lc_letters:
+                                                for k in lc_letters:
+                                                    word = a+b+c+d+e+f+g+h+i+j+k
+                                                    file.write(word+"\n")
+        print "DONE"
+        file.close()
+    except KeyboardInterrupt:
+        print "KeyboardInterrupt"
+elif lenght == "12":
+    file = open("DIC_lcletters_12.txt","w")
+    try:
+        for a in lc_letters:
+            print a
+            for b in lc_letters:
+                for c in lc_letters:
+                    for d in lc_letters:
+                        for e in lc_letters:
+                            for f in lc_letters:
+                                for g in lc_letters:
+                                    for h in lc_letters:
+                                        for i in lc_letters:
+                                            for j in lc_letters:
+                                                for k in lc_letters:
+                                                    for l in lc_letters:
+                                                        word = a+b+c+d+e+f+g+h+i+j+k+l
+                                                        file.write(word+"\n")
+        print "DONE"
+        file.close()
+    except KeyboardInterrupt:
+        print "KeyboardInterrupt"
+elif lenght == "13":
+    file = open("DIC_lcletters_13.txt","w")
+    try:
+        for a in lc_letters:
+            print a
+            for b in lc_letters:
+                for c in lc_letters:
+                    for d in lc_letters:
+                        for e in lc_letters:
+                            for f in lc_letters:
+                                for g in lc_letters:
+                                    for h in lc_letters:
+                                        for i in lc_letters:
+                                            for j in lc_letters:
+                                                for k in lc_letters:
+                                                    for l in lc_letters:
+                                                        for m in lc_letters:
+                                                            word = a+b+c+d+e+f+g+h+i+j+k+l+m
+                                                            file.write(word+"\n")
+        print "DONE"
+        file.close()
+    except KeyboardInterrupt:
+        print "KeyboardInterrupt"
+elif lenght == "14":
+    file = open("DIC_lcletters_14.txt","w")
+    try:
+        for a in lc_letters:
+            print a
+            for b in lc_letters:
+                for c in lc_letters:
+                    for d in lc_letters:
+                        for e in lc_letters:
+                            for f in lc_letters:
+                                for g in lc_letters:
+                                    for h in lc_letters:
+                                        for i in lc_letters:
+                                            for j in lc_letters:
+                                                for k in lc_letters:
+                                                    for l in lc_letters:
+                                                        for m in lc_letters:
+                                                            for n in lc_letters:
+                                                                word = a+b+c+d+e+f+g+h+i+j+k+l+m+n
+                                                                file.write(word+"\n")
+        print "DONE"
+        file.close()
+    except KeyboardInterrupt:
+        print "KeyboardInterrupt"
+elif lenght == "15":
+    file = open("DIC_lcletters_15.txt","w")
+    try:
+        for a in lc_letters:
+            print a
+            for b in lc_letters:
+                for c in lc_letters:
+                    for d in lc_letters:
+                        for e in lc_letters:
+                            for f in lc_letters:
+                                for g in lc_letters:
+                                    for h in lc_letters:
+                                        for i in lc_letters:
+                                            for j in lc_letters:
+                                                for k in lc_letters:
+                                                    for l in lc_letters:
+                                                        for m in lc_letters:
+                                                            for n in lc_letters:
+                                                                for o in lc_letters:
+                                                                    word = a+b+c+d+e+f+g+h+i+j+k+l+m+n+o
+                                                                    file.write(word+"\n")
+        print "DONE"
+        file.close()
+    except KeyboardInterrupt:
+        print "KeyboardInterrupt"
+elif lenght == "16":
+    file = open("DIC_lcletters_16.txt.txt","w")
+    try:
+        for a in lc_letters:
+            print a
+            for b in lc_letters:
+                for c in lc_letters:
+                    for d in lc_letters:
+                        for e in lc_letters:
+                            for f in lc_letters:
+                                for g in lc_letters:
+                                    for h in lc_letters:
+                                        for i in lc_letters:
+                                            for j in lc_letters:
+                                                for k in lc_letters:
+                                                    for l in lc_letters:
+                                                        for m in lc_letters:
+                                                            for n in lc_letters:
+                                                                for o in lc_letters:
+                                                                    for p in lc_letters:
+                                                                        word = a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p
+                                                                        file.write(word+"\n")
+        print "DONE"
+        file.close()
+    except KeyboardInterrupt:
+        print "KeyboardInterrupt"
+else:
+    print "Erreur"
+
+totalTime = time.time() - start_time
+print totalTime
